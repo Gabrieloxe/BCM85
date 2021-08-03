e628d5b16d34c6b98e9c5f406a06fe7b77e09047
Johnathan Wong <biz.johnathanwongwh@gmail.com>
2021-08-03 23:34:29 +0800

    Set up api & mongo

---
+++ api/src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+require("./config/db")();
+
+const api = require("./api");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/healthz", async (req, res) =>
+  res.status(200).send({ message: "Health check", status: true }),
+);
+
+app.get("/", async (req, res) =>
+  res.status(200).send({ message: "Express api ready", status: true }),
+);
+
+app.use("/api/v1", api);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server started and istening on ${port}`));
---
+++ api/src/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { mongoURI } = require("./default.js");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    console.log("MongoDB Connection Failed...");
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
