f1e14780e71da8eaad3d467b3a9e294b3f8c24dc
Johnathan Wong <biz.johnathanwongwh@gmail.com>
2021-08-07 23:04:25 +0800

    Add 404 route, organize app.js

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -1,7 +1,15 @@
+const missingRoute = require("../middlewares/404");
 module.exports = (app) => {
+  app.use(missingRoute);
+  app.get("/healthz", async (req, res) =>
+    res.status(200).send({ message: "Health check", status: true }),
+  );
+  app.get("/", async (req, res) =>
+    res.status(200).send({ message: "Express api ready", status: true }),
+  );
   app.use("/api/beloveds", require("./beloveds"));
   app.use("/api/events", require("./event"));
-  app.get("/api", (req, res) => {
-    res.send({ message: "Express api health check!" });
-  });
+  app.get("/api", (req, res) =>
+    res.send({ message: "Express api health check!" }),
+  );
 };
--- api/src/app.js
+++ api/src/app.js
@@ -2,10 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+
 require("./config/db")();
-
-const api = require("./api");
-
 const app = express();
 
 app.use(morgan("dev"));
@@ -13,13 +11,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get("/healthz", async (req, res) =>
-  res.status(200).send({ message: "Health check", status: true }),
-);
-
-app.get("/", async (req, res) =>
-  res.status(200).send({ message: "Express api ready", status: true }),
-);
+app.use('')
 
 require("./api")(app);
 
