5dabe614955a50ab4d2769bd5bacd02a08068219
Gabriel Ong <gabrielongxe@gmail.com>
2021-08-07 14:39:42 +0800

    beloved mongo schema

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.json({
+router.get("/", async (request, response) => {
+  response.json({
     message: "API - 👋🌎🌍🌏",
   });
 });
---
+++ api/src/models/beloveds.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const db = require('../api/config/db.js');
+
+console.log('connecting to', url);
+
+db.connnectDB();
+
+const belovedSchema = new mongoose.Schema({
+  name: String,
+  date: Date,
+});
+
+belovedSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Beloved', belovedSchema);
